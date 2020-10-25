fdf8108308bf4cd534673108543a0e37a4fe3d03
DanielLevi3 <66633430+DanielLevi3@users.noreply.github.com>
2020-10-25 20:38:11 +0200

    Add files via upload
    חלק 3 שאלה 2

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPart3Question2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rational test1 = new Rational(4, 16);
+            Rational test2 = new Rational(15, 3);
+            Rational r = test1 + test2;
+            Console.WriteLine(r);
+            Rational r2 = test1 - test2;
+            Console.WriteLine(r2);
+            bool b = test2 > test1;
+            Console.WriteLine(b);
+            bool d = test1 > test2;
+            Console.WriteLine(d);
+            Rational r3 = test1 * test2;
+            Console.WriteLine(r3);
+
+        }
+    }
+}
---
+++ Rational.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPart3Question2
+{
+    class Rational
+    {
+        public int Mone { get; set; }
+        public int Mehane { get; set; }
+        public Rational(int mone, int mehane)
+        {
+            Mone = mone;
+            if (mehane < 0)
+            {
+                Mehane = Math.Abs(mehane);
+            }
+            else
+                Mehane = mehane;
+        }
+        public static double TurnToRational(Rational r1)
+        {
+            double d = r1.Mone / r1.Mehane;
+            return d;
+        }
+        public static bool operator >(Rational r1,Rational r2)
+        {
+            double ratio1 = TurnToRational(r1);
+            double ratio2 = TurnToRational(r2);
+            return ratio1 > ratio2;
+        }
+        public static bool operator <(Rational r1, Rational r2)
+        {
+            double ratio1 = TurnToRational(r1);
+            double ratio2 = TurnToRational(r2);
+            return ratio1 < ratio2;
+        }
+        public static bool operator ==(Rational r1, Rational r2)
+        {
+            if (r1 == null && r2 == null)
+                return true;
+            if (r1 == null || r2 == null)
+                return false;
+            return r1 == r2;
+        }
+        public static bool operator !=(Rational r1, Rational r2)
+        { 
+            return !(r1== r2);
+        }
+        public static Rational operator +(Rational r1 , Rational r2)
+        {
+            Rational res = new Rational(r1.Mone + r2.Mone, r1.Mehane + r2.Mehane);
+            return res;
+        }
+        public static Rational operator -(Rational r1, Rational r2)
+        {
+            Rational res = new Rational(Math.Abs(r1.Mone - r2.Mone),Math.Abs(r1.Mehane - r2.Mehane));
+            return res;
+        }
+        public static Rational operator *(Rational r1, Rational r2)
+        {
+            Rational res = new Rational(r1.Mone * r2.Mone, r1.Mehane * r2.Mehane);
+            return res;
+        }
+        public int getNumerator(Rational r1)
+        {
+            return r1.Mone;
+        }
+        public int getDenominator(Rational r1)
+        {
+            return r1.Mehane;
+        }
+        public override bool Equals(object obj)
+        {
+            return obj is Rational rational &&
+                   Mone == rational.Mone &&
+                   Mehane == rational.Mehane;
+        }
+        public override int GetHashCode()
+        {
+            int hashCode = -1129719267;
+            hashCode = hashCode * -1521134295 + Mone.GetHashCode();
+            hashCode = hashCode * -1521134295 + Mehane.GetHashCode();
+            return hashCode;
+        }
+        public override string ToString()
+        {
+            return $"{Mone}/{Mehane}";
+        }
+    }
+}
